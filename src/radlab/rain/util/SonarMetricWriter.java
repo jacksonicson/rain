545b3cef931cd8b78b5da379a05786b1bf215b6d
Andreas Wolke <jacksonicson@gmail.com>
2012-08-29 10:21:15 +0200

    Refactoring Scoreboard class

---
+++ src/radlab/rain/util/SonarMetricWriter.java
@@ -0,0 +1,73 @@
+package radlab.rain.util;
+
+import java.net.InetAddress;
+
+import org.apache.log4j.Logger;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.json.JSONObject;
+
+import radlab.rain.ResponseTimeStat;
+import de.tum.in.sonar.collector.CollectService;
+import de.tum.in.sonar.collector.Identifier;
+import de.tum.in.sonar.collector.MetricReading;
+
+public class SonarMetricWriter extends MetricWriter {
+
+	private static final Logger logger = Logger.getLogger(SonarMetricWriter.class);
+
+	private final String HOSTNAME;
+
+	private CollectService.Client client;
+	private TTransport transport;
+
+	public SonarMetricWriter(JSONObject config) throws Exception {
+		super(config);
+		// Read configuration
+		String sonarServer = config.getString("sonarServer");
+		logger.debug("sonar server: " + sonarServer);
+
+		// Get hostname
+		InetAddress addr = InetAddress.getLocalHost();
+		this.HOSTNAME = addr.getHostName();
+
+		// Get Sonar connection
+		transport = new TSocket(sonarServer, 7921);
+		transport.open();
+
+		TProtocol protocol = new TBinaryProtocol(transport);
+
+		// Create new client
+		client = new CollectService.Client(protocol);
+	}
+
+	@Override
+	public String getDetails() {
+		return "SonarMetricWriter";
+	}
+
+	@Override
+	public boolean write(ResponseTimeStat stat) throws Exception {
+		logger.debug("response_time " + stat._responseTime + " " + HOSTNAME + " " + stat._timestamp);
+
+		Identifier id = new Identifier();
+		id.setHostname(HOSTNAME);
+		id.setSensor("specj.rtime");
+		id.setTimestamp(stat._timestamp / 1000);
+
+		MetricReading value = new MetricReading();
+		value.setValue(stat._responseTime);
+
+		client.logMetric(id, value);
+
+		return true;
+	}
+
+	@Override
+	public void close() throws Exception {
+		transport.close();
+	}
+
+}
