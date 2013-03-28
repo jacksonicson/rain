7779a14d50885e2aa6203dde2d8c6170afd50000
Andreas Wolke <jacksonicson@gmail.com>
2013-03-28 17:48:27 +0100

    removed workloads. New rain interfaces are incompatible with this implementations.

--- workloads/radlab/rain/workload/mongodb/MongoRequest.java
+++
@@ -1,9 +0,0 @@
-package radlab.rain.workload.mongodb;
-
-public class MongoRequest<T> 
-{
-	public T key;
-	public int op;
-	public int size;
-	public byte[] value;
-}
--- workloads/radlab/rain/workload/riak/RiakRequest.java
+++
@@ -1,9 +0,0 @@
-package radlab.rain.workload.riak;
-
-public class RiakRequest<T> 
-{
-	public T key;
-	public int op;
-	public int size;
-	public byte[] value;
-}
