e3fcf2ea4baf12152f3524b85e04aa58b4a5a6e9
Andreas Wolke <jacksonicson@gmail.com>
2013-03-30 20:56:10 +0100

    documentation operation

--- src/radlab/rain/operation/IOperation.java
+++ src/radlab/rain/operation/IOperation.java
@@ -4,22 +4,30 @@ import radlab.rain.load.LoadDefinition;
 
 public interface IOperation extends Runnable {
 
+	// Reference to the load definition during which the operation was created
 	void setLoadDefinition(LoadDefinition loadDefinition);
 
+	// Return the load definition. This is used to rate limit operation execution.
 	LoadDefinition getLoadDefinition();
 
+	// Internal operation index
 	public int getOperationIndex();
-	
+
+	// Internal operation name
 	public String getOperationName();
 
+	// Execute this operation in asynchronous or synchronous mode
 	boolean isAsync();
 
-	void setAsync(boolean async);
-
+	// Force synchronous operation
 	boolean isForceSync();
 
+	// Update execution sate if it was changed (e.g. by open loop load generation)
+	void setAsync(boolean async);
+
+	// Prepare before executing
 	void prepare();
 
+	// Cleanup after execution
 	void cleanup();
-
 }
